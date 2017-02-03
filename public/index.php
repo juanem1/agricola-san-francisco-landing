f9f65287de114e72aec5d08802d2d1e4cd6a2fe0
Juan Emilio Turk <juanemilioturk@gmail.com>
2017-02-03 23:57:21 +0000

    index.php edited online with Bitbucket

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
-
-<meta name="google-site-verification" content="6qWY1L39LXBBdhRdYeo0QPCLyLUhlqJa_c9sNFBpT00" />
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Agricolasanfrancisco.com</title>
+    <meta name="google-site-verification" content="6qWY1L39LXBBdhRdYeo0QPCLyLUhlqJa_c9sNFBpT00" />
+  </head>
+  <body>
+  </body>
+</html>
