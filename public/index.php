47a3340e305ec5a0f08ffe0fa3cc50c9151f14ab
Juan Emilio Turk <juanemilioturk@gmail.com>
2017-02-07 00:39:24 -0300

    new landing version 2

--- public/index.php
+++ public/index.php
@@ -1,11 +1,3 @@
-<?php
- $image = '/img/1.jpg';
- $image = (isset($_GET['bg']) && $_GET['bg'] === '2') ? '/img/2.jpg' : $image;
- $image = (isset($_GET['bg']) && $_GET['bg'] === '3') ? '/img/3.jpg' : $image;
- $image = (isset($_GET['bg']) && $_GET['bg'] === '4') ? '/img/4.jpg' : $image;
- $image = (isset($_GET['bg']) && $_GET['bg'] === '5') ? '/img/5.jpg' : $image;
- $image = (isset($_GET['bg']) && $_GET['bg'] === '6') ? '/img/6.jpg' : $image;
-?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -14,26 +6,79 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="iBox Design Studio. ideasbox.com.ar">
-    <title>Agricolasanfrancisco.com</title>
+    <title>Agricola San Francisco</title>
     <link href="/img/favicon.png" rel="shortcut icon">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="/css/bulma.css" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
   </head>
   <body>
-    <section class="hero is-fullheight" style="background-image:url('<?php echo $image ?>')">
-      <div class="overlay">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="title">
-              Agricola San Francisco
-            </h1>
-            <h2 class="subtitle">
-              Proximamente...
-            </h2>
+    <nav class="nav">
+      <div class="nav-center">
+        <a class="nav-item">
+          <img src="/img/logo.png" alt="Agricola San Francisco logo">
+        </a>
+      </div>
+    </nav>
+
+    <div class="banner"></div>
+
+    <div class="columns">
+      <div class="column has-text-centered is-one-third">
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-96x96">
+              <img src="/img/2a.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <div class="wrapper">
+                <strong class="a2">Ruben Alonso</strong><br> 
+                <div class="a2">Director</div> 
+                <a href="&#x6d;&#x61;&#105;&#108;&#x74;&#111;&#x3a;&#x72;&#117;&#x62;&#x65;&#x6e;&#x61;&#108;&#x6f;&#110;&#x73;&#x6f;&#64;&#97;&#103;&#x72;&#x69;&#x63;&#111;&#108;&#x61;&#x73;&#97;&#110;&#x66;&#x72;&#x61;&#x6e;&#99;&#x69;&#115;&#x63;&#x6f;&#x2e;&#x63;&#111;&#109;">&#114;&#117;&#x62;&#101;&#110;&#97;&#x6c;&#x6f;&#110;&#x73;&#111;&#x40;&#97;&#103;&#x72;&#105;&#x63;&#x6f;&#108;&#x61;&#x73;&#x61;&#x6e;&#102;&#x72;&#x61;&#110;&#x63;&#105;&#x73;&#99;&#111;&#46;&#99;&#111;&#x6d;</a>
+              </div>
+            </div>
           </div>
+        </article>
       </div>
+      <div class="column has-text-centered is-one-third">
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-96x96">
+              <img src="/img/3a.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <div class="wrapper">
+                <strong class="a3">Ramiro Alonso</strong><br> 
+                <div class="a3">Commercial Manager</div>
+                <a href="&#x6d;&#97;&#105;&#x6c;&#116;&#x6f;&#58;&#x72;&#x61;&#109;&#x69;&#x72;&#111;&#x61;&#x6c;&#x6f;&#x6e;&#115;&#x6f;&#64;&#97;&#x67;&#114;&#105;&#99;&#111;&#108;&#x61;&#115;&#97;&#x6e;&#x66;&#x72;&#97;&#110;&#x63;&#105;&#115;&#x63;&#111;&#46;&#99;&#x6f;&#x6d;">&#114;&#x61;&#109;&#x69;&#114;&#111;&#97;&#108;&#x6f;&#x6e;&#x73;&#x6f;&#x40;&#97;&#103;&#x72;&#105;&#99;&#x6f;&#x6c;&#97;&#115;&#x61;&#x6e;&#102;&#114;&#x61;&#110;&#99;&#x69;&#115;&#x63;&#x6f;&#x2e;&#99;&#x6f;&#109;</a>
+              </div>
+            </div>
+          </div>
+        </article>
       </div>
-    </section>
+      <div class="column has-text-centered is-one-third">
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-96x96">
+              <img src="/img/1a.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <div class="wrapper">
+                <strong class="a1">Gonzalo Alonso</strong><br> 
+                <div class="a1">Commercial Manager</div>
+                <a href="&#x6d;&#x61;&#x69;&#x6c;&#116;&#111;&#58;&#103;&#x6f;&#110;&#122;&#x61;&#108;&#111;&#x61;&#108;&#x6f;&#110;&#x73;&#x6f;&#x40;&#x61;&#x67;&#114;&#105;&#99;&#x6f;&#x6c;&#x61;&#x73;&#97;&#110;&#102;&#x72;&#97;&#x6e;&#99;&#105;&#115;&#x63;&#x6f;&#x2e;&#99;&#x6f;&#x6d;">&#103;&#x6f;&#110;&#122;&#97;&#x6c;&#111;&#x61;&#108;&#111;&#110;&#x73;&#x6f;&#x40;&#x61;&#x67;&#x72;&#105;&#99;&#x6f;&#x6c;&#x61;&#115;&#97;&#x6e;&#102;&#x72;&#97;&#x6e;&#99;&#x69;&#x73;&#x63;&#111;&#46;&#x63;&#x6f;&#109;</a>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+    <div class="footer"></div>
   </body>
 </html>
